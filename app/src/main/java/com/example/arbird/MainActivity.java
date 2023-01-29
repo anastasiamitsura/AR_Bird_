68aae5baa4a17b5918e2c86abaa93561dd258c9e
Nastya_Mits <nastamicura2@gmail.com>
2023-01-29 12:26:06 +0700

    Toolbar normal commit

--- app/src/main/java/com/example/arbird/MainActivity.java
+++ app/src/main/java/com/example/arbird/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.arbird;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.arbird.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.camerabt, R.id.historybt, R.id.profilebt)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navV, navController);
+
 
         int a = 0;
     }
